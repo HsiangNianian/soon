235acea75f2a841d941753cd98d133ce0f17cf74
HsiangNianian <i@jyunko.cn>
2025-05-25 03:13:42 +0800

    feat:  Update version to 0.1.2 in .SRCINFO and source URL

--- scripts/check.sh
+++ scripts/check.sh
@@ -2,3 +2,4 @@ for hash in $(git rev-list --max-count=10 HEAD); do
   echo "Checking $hash..."
   git ls-tree --name-only -r $hash | grep -q '^PKGBUILD$' || echo "❌ Missing PKGBUILD in $hash"
 done
+makepkg --printsrcinfo > .SRCINFO
