da57e4f6ea6b2f7defc9d207aa50b3fd6ac5e38f
HsiangNianian <i@jyunko.cn>
2025-05-25 10:58:19 +0800

    feat: Bump version to 0.1.4 in Cargo.toml and PKGBUILD; clean up check.sh

--- scripts/check.sh
+++ scripts/check.sh
@@ -1,9 +1,7 @@
+cd ../aur
 for hash in $(git rev-list --max-count=10 HEAD); do
   echo "Checking $hash..."
   git ls-tree --name-only -r $hash | grep -q '^PKGBUILD$' || echo "❌ Missing PKGBUILD in $hash"
 done
-<<<<<<< HEAD
 makepkg --printsrcinfo > .SRCINFO
-=======
-makepkg --printsrcinfo > .SRCINFO
->>>>>>> dab388da8e452a4f82f1297bf20c48254028d1c1
+cd ..
